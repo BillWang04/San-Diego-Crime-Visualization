700970d8971f61c66a2361b68ccf9b1061ae0bf9
Bill Wang <18billwang@gmail.com>
2024-02-18 17:48:02 -0800

    trying to find a way to add legend/sidebar

--- crimes_data/src/App.svelte
+++ crimes_data/src/App.svelte
@@ -1,54 +1,92 @@
 <script>
-    mapboxgl.accessToken = "pk.eyJ1IjoiYmx3MDAyIiwiYSI6ImNsc21kam9xcTBtaHQycXBkNGVudTZmNXAifQ.Zxv8Wui2RBVxFDRB0mbvMw";
-	const map = new mapboxgl.Map({
-		container: "map",
-		style: "mapbox://styles/mapbox/dark-v11", 
-		center: [-117.1611, 32.7157], 
-		zoom: 12, // starting zoom level
-		minZoom: 11,
-		maxZoom: 20,
-	});
+    import { onMount } from "svelte";
+	import "../node_modules/mapbox-gl/dist/mapbox-gl.css"
+
+	let mapContainer;
+	let lng, lat, zoom;
+
+	lng = -117.1611;
+	lat = 32.7157;
+	zoom = 11;
 	//test data
 	const dataPoints = [
       { coordinates: [-117.1611, 32.7157], title: "Point 1", type: "Theft", date: "2024-02-17", description: "Details about the crime" },
       { coordinates: [-117.1701, 32.7105], title: "Point 2", type: "Assault", date: "2024-02-18", description: "Details about the crime" },
     ];
 
+	let crimeFilter = {};
 
 	//colors of categories
 	const crimeColors = {
-      Theft: 'blue',
-      Assault: 'red',
+      	Theft: 'blue',
+     	Assault: 'red',
     };
 
-	dataPoints.forEach(point => {
-      const marker = new mapboxgl.Marker({
-        color: crimeColors[point.type] || 'gray', // Default to gray if no color is defined
-      })
-        .setLngLat(point.coordinates)
-        .setPopup(new mapboxgl.Popup().setHTML(`
-          <h3>${point.title}</h3>
-          <p><strong>Type:</strong> ${point.type}</p>
-          <p><strong>Date:</strong> ${point.date}</p>
-          <p><strong>Description:</strong> ${point.description}</p>
-        `))
-        .addTo(map);
-    });
+	let markers = [];
+	onMount(() =>{
+		mapboxgl.accessToken = "pk.eyJ1IjoiYmx3MDAyIiwiYSI6ImNsc21kam9xcTBtaHQycXBkNGVudTZmNXAifQ.Zxv8Wui2RBVxFDRB0mbvMw";
+
+		const map = new mapboxgl.Map({
+			container: "map",
+			style: "mapbox://styles/mapbox/dark-v11", 
+			center: [lng, lat], 
+			zoom: zoom, // starting zoom level
+			minZoom: 11,
+			maxZoom: 18,
+		});
+
+		dataPoints.forEach(point => {
+		const marker = new mapboxgl.Marker({
+			color: crimeColors[point.type] || 'gray', // Default to gray if no color is defined
+		})
+			.setLngLat(point.coordinates)
+			.setPopup(new mapboxgl.Popup().setHTML(`
+			<h3>${point.title}</h3>
+			<p><strong>Type:</strong> ${point.type}</p>
+			<p><strong>Date:</strong> ${point.date}</p>
+			<p><strong>Description:</strong> ${point.description}</p>
+			`))
+			.addTo(map);
+		});
+	});
+
 
 
 
 </script>
 
 <main>
+	<div class =legend>
+		Longitude: {lng.toFixed(4)} | Latitude: {lat.toFixed(4)} | Zoom: {zoom.toFixed(2)}
+	</div>
+	<div class="map-wrap">
+		<div class="map" bind:this={mapContainer} />
+	  </div>
+	  
 
 </main>
 
 <style>
-	#map {
-	  height: 400px; /* Set the desired height for your map */
-	  width: 100%;
+	.map {
+	position: absolute;
+	width: 100%;
+	height: 100%;
 	}
-  
+	.sidebar {
+	background-color: rgb(35 55 75 / 90%);
+	color: #fff;
+	padding: 6px 12px;
+	font-family: monospace;
+	z-index: 1;
+	position: absolute;
+	top: 0;
+	left: 0;
+	margin: 12px;
+	border-radius: 4px;
+	}
+
+
+
 
   </style>
 
